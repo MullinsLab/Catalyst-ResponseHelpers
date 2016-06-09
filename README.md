d1ab96053474ee391bbe876b519cf5556f5f935f
Thomas Sibley <trsibley@uw.edu>
2016-06-09 16:36:26 -0700

    Milla skeleton

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Catalyst::ResponseHelpers - Blah blah blah
+
+# SYNOPSIS
+
+    use Catalyst::ResponseHelpers;
+
+# DESCRIPTION
+
+Catalyst::ResponseHelpers is
+
+# AUTHOR
+
+Thomas Sibley <trsibley@uw.edu>
+
+# COPYRIGHT
+
+Copyright 2016- Thomas Sibley
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Catalyst/ResponseHelpers.pm
@@ -0,0 +1,39 @@
+package Catalyst::ResponseHelpers;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Catalyst::ResponseHelpers - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Catalyst::ResponseHelpers;
+
+=head1 DESCRIPTION
+
+Catalyst::ResponseHelpers is
+
+=head1 AUTHOR
+
+Thomas Sibley E<lt>trsibley@uw.eduE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2016- Thomas Sibley
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
